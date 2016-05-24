99c94f848d12cf9bd71edb11a93388ab97b329a3
Joao Machado <machado@codacy.com>
2016-05-24 18:56:09 +0100

    Only checks python2 files

--- src/main/scala/codacy/bandit/Bandit.scala
+++ src/main/scala/codacy/bandit/Bandit.scala
@@ -9,6 +9,9 @@ import play.api.libs.json.Json
 import scala.io.Source
 import scala.util.Try
 
+private case class FilesByVersion(python2: List[String], python3: List[String])
+private case class ClassifiedLine(filename: String, pythonVersion: Int)
+
 object Bandit extends Tool {
 
   override def apply(path: Path, conf: Option[List[PatternDef]], files: Option[Set[Path]])(implicit spec: Spec): Try[List[Result]] = {
@@ -24,8 +27,52 @@ object Bandit extends Tool {
           patternDef.map(_.patternId).toSet
       }
 
+      val filesByVersion = partitionFilesByPythonVersion(filesToLint)
+
+      runTool("bandit", filesByVersion.python2, enabledPatterns)
+    }
+  }
+
+  private val ClassifiedLineRegex = """(.*)###(\d)""".r
+
+  private def toClassifiedLine(line: String): ClassifiedLine = {
+    line match {
+      case ClassifiedLineRegex(filename, pythonVersion) =>
+        ClassifiedLine(filename, pythonVersion.toInt)
+    }
+  }
+
+  private def partitionFilesByPythonVersion(filesToLint: List[String]): FilesByVersion = {
+    /*
+     * The classifyScript.py will return one line per file in fileName###PythonVersion format
+     * Example: B104.py###2
+     */
+    val command = List("python", "classifyScript.py") ++ filesToLint
+
+    CommandRunner.exec(command) match {
+      case Right(res) if res.exitCode == 0 =>
+        val (python2, python3) = res.stdout.map(toClassifiedLine).partition(_.pythonVersion == 2)
+        FilesByVersion(python2.map(_.filename), python3.map(_.filename))
+
+      case Right(resultFromTool) if resultFromTool.exitCode > 0 =>
+        throw new scala.Exception(
+          s"[ClassifyScript]\n" +
+          s"Exit code: ${resultFromTool.exitCode}\n" +
+            s"stdout: ${resultFromTool.stdout}\n" +
+            s"sterr: ${resultFromTool.stdout}\n"
+        )
+      case Left(failure) =>
+        throw failure
+    }
+  }
+
+  private def runTool(toolName: String, filesToLint: List[String], enabledPatterns: => Set[PatternId]): List[Result] = {
+    if(filesToLint.isEmpty) {
+      List.empty[Result]
+    }
+    else {
       val toolResultPath = "/tmp/bandit-out.json"
-      val command = List("bandit", "-f", "json", "-o", toolResultPath) ++ filesToLint
+      val command = List(toolName, "-f", "json", "-o", toolResultPath) ++ filesToLint
 
       CommandRunner.exec(command) match {
         case Right(resultFromTool) if resultFromTool.exitCode <= 1 =>
@@ -34,8 +81,8 @@ object Bandit extends Tool {
             .filter(result => resultFilter(result, enabledPatterns))
 
         case Right(resultFromTool) if resultFromTool.exitCode > 1 =>
-          throw new Exception(
-            s"Error code: ${resultFromTool.exitCode}\n" +
+          throw new scala.Exception(
+            s"Exit code: ${resultFromTool.exitCode}\n" +
               s"stdout: ${resultFromTool.stdout}\n" +
               s"sterr: ${resultFromTool.stdout}\n"
           )
