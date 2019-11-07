6f6cc15b34591cf5f4749d8e6463ff2976c0ebd2
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-11-07 15:54:47 +0100

    fix: Fix some patterns title
    - Documentation generator creates files and directories in case they don't exist

--- doc-generator/src/main/scala/docs/transformers/BlacklistDocTransformer.scala
+++ doc-generator/src/main/scala/docs/transformers/BlacklistDocTransformer.scala
@@ -30,20 +30,23 @@ object BlacklistDocTransformer extends IPatternDocTransformer {
     * Example:
     * <div class="section" id="b304-b305-ciphers-and-modes">
     */
-  private def patternIds(body: Node) = {
+  private def patternIds(body: Node): Seq[String] = {
     (body \ "@id").text match {
       case patternIdIntervalRegex(c) =>
         val Array(firstPatternIdStr, lastPatternIdStr) = c.replace("b", "").split("-")
-        val pIdsList = (firstPatternIdStr.toInt to lastPatternIdStr.toInt)
-        pIdsList.map("b" + _.toString)
-      case patternIdRegex(c) => List(c)
-      case _ => List.empty
+        val pIds = firstPatternIdStr.toInt.to(lastPatternIdStr.toInt)
+        pIds.map("b" + _.toString)
+      case patternIdRegex(c) => Seq(c)
+      case _ => Seq.empty
     }
   }
 
   /** Get the pattern title from the first paragraph*/
-  private def getTitle(body: Node) = {
-    val bulk_title = (body \ "p").text.replace("\n", " ")
+  private def getTitle(body: Node, patternId: String) = {
+    val isB313B320 = 313.to(320).contains(patternId.tail.toInt) 
+    val pNodes = (body \ "p")
+    val node = if(isB313B320) pNodes(1) else pNodes(0)
+    val bulk_title = node.text.replace("\n", " ")
     bulk_title.split("\\. ").head
   }
 
@@ -56,9 +59,9 @@ object BlacklistDocTransformer extends IPatternDocTransformer {
       htmlFiles <- sourceDirectory.listRecursively.toSeq
       htmlPluginsDocs <- Pandoc.loadHtml(htmlFiles)
       body <- patternsDocumentationBody(htmlPluginsDocs)
-      title = getTitle(body)
       descriptionText = Pandoc.convert(body.toString())
       patternId <- patternIds(body)
+      title = getTitle(body, patternId)
     } yield Pattern(patternId.capitalize, title, descriptionText, Level.Warn, Category.Security)
   }
 }
