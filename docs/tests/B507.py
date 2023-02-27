badffb81b23cc6a34862e75437e1e1784656ee27
Stefan Vacareanu <stefan@codacy.com>
2023-02-27 12:15:17 +0800

    Remove depecreated rule

--- docs/tests/B507.py
+++
@@ -1,9 +0,0 @@
-##Patterns: B507
-
-from paramiko import client
-
-ssh_client = client.SSHClient()
-##Err: B507
-ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
-##Err: B507
-ssh_client.set_missing_host_key_policy(client.WarningPolicy)
