6965ee8e371015f22482423bad45499c17e692ed
Stefan Vacareanu <stefan@codacy.com>
2023-09-11 11:57:36 +0300

    Update tests

--- docs/tests/B507.py
+++ docs/tests/B507.py
@@ -3,7 +3,7 @@
 from paramiko import client
 
 ssh_client = client.SSHClient()
-##Err: B507
+##Warn: B507
 ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
-##Err: B507
+##Warn: B507
 ssh_client.set_missing_host_key_policy(client.WarningPolicy)
