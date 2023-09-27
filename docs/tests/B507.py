68fd17baa0c645793233c755dd2319e05bb186bf
Stefan Vacareanu <stefan@codacy.com>
2023-09-27 16:35:39 +0300

    Change the documentation TS-595

--- docs/tests/B507.py
+++ docs/tests/B507.py
@@ -3,7 +3,7 @@
 from paramiko import client
 
 ssh_client = client.SSHClient()
-##Warn: B507
+##Err: B507
 ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
-##Warn: B507
+##Err: B507
 ssh_client.set_missing_host_key_policy(client.WarningPolicy)
